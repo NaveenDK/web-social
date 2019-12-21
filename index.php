3662d189da25130659ec6dc3e12214d0aebb3e43
naveendk <naveenkolambage@gmail.com>
2019-12-21 13:58:19 +1300

    Added user_details block, dynamically pull the no-of-posts,likes and the name-mostly done changes on style.css

--- index.php
+++ index.php
@@ -2,9 +2,15 @@
 include("includes/header.php");
 ?>
     <div class="user_details column">
-    <a href="">  <img width="100" src="<?php echo $user['profile_pic']; ?>"/> </a>
+    <a href="">  <img width="80"src="<?php echo $user['profile_pic']; ?>"/> </a>
 
+    <div class="user_details_left_right">
+     <a href=""> <?php echo $user['first_name']." ".$user['last_name']; ?> </a> <br>
+     <?php echo "Posts: ".$user['num_posts']."<br>";
+        echo "Likes: ".$user['num_likes'];
+     ?>   
     </div>
-</div>
+    </div> <!-- closes the div tag that is opened with the class user_details column -->
+</div> <!--closes the div tag opened in the wrapper at the header.php tag -->
 </body>
 </html>
